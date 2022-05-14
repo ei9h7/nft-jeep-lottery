581308c0ee8965c178ef11cd1d8831d2be848f35
Ayton MacEachern <8theVirulent@gmail.com>
2022-05-14 16:39:07 -0600

    update mumbai rpcUrl

--- src/config/polygonChain.config.ts
+++ src/config/polygonChain.config.ts
@@ -2,7 +2,8 @@ const PolygonChainInfo = {
   name: "Mumbai Polygon",
   chainId: 80001,
   hexChaindId: "0x13881",
-  rpcUrl: "https://rpc-mumbai.maticvigil.com/",
+  rpcUrl:
+    "https://polygon-mumbai.infura.io/v3/ff5fcf5f0d404ecab04250c349f1afed",
   blockExplorer: "https://mumbai.polygonscan.com",
   contractAddress: "0x9eEbeE5010Bbe863C223c89E2a43D950805135e9",
 };
